68233da67b07f956445be6ff938f808c0d010d89
Destia <tobingdestia734@gmail.com>
2024-03-23 17:03:33 +0700

    feat: api integration create account

---
+++ src/const.js
@@ -0,0 +1,1 @@
+export const API_URL='https://pbl-micro-learling.up.railway.app/api'
--- src/screens/Register/Register.tsx
+++ src/screens/Register/Register.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import {
 	View,
 	ActivityIndicator,
@@ -7,30 +7,32 @@ import {
 	ScrollView,
 	Alert,
 	TextInput,
-} from 'react-native';
-import i18next from 'i18next';
-import { useTranslation } from 'react-i18next';
-import { useQuery } from '@tanstack/react-query';
+	Button,
+} from "react-native";
+import i18next from "i18next";
+import { useTranslation } from "react-i18next";
+import { useQuery } from "@tanstack/react-query";
 
-import { ImageVariant } from '@/components/atoms';
-import { Brand } from '@/components/molecules';
-import { SafeScreen } from '@/components/template';
-import { useTheme } from '@/theme';
-import { fetchOne } from '@/services/users';
+import { ImageVariant } from "@/components/atoms";
+import { Brand } from "@/components/molecules";
+import { SafeScreen } from "@/components/template";
+import { useTheme } from "@/theme";
+import { fetchOne } from "@/services/users";
 
-import { isImageSourcePropType } from '@/types/guards/image';
+import { isImageSourcePropType } from "@/types/guards/image";
 
-import SendImage from '@/theme/assets/images/send.png';
-import ColorsWatchImage from '@/theme/assets/images/colorswatch.png';
-import TranslateImage from '@/theme/assets/images/translate.png';
-import SelectDropdown from 'react-native-select-dropdown'
-const genders = ["Pria", "Wanita"]
+import SendImage from "@/theme/assets/images/send.png";
+import ColorsWatchImage from "@/theme/assets/images/colorswatch.png";
+import TranslateImage from "@/theme/assets/images/translate.png";
+import SelectDropdown from "react-native-select-dropdown";
+import { API_URL } from "@/const";
+const genders = ["Pria", "Wanita"];
 
 function Example() {
-	const { t } = useTranslation(['example', 'welcome']);
-	const [name, onChangeName] = useState('');
-	const [email, onChangeEmail] = useState('');
-	const [password, onChangePassword] = useState('');
+	const { t } = useTranslation(["example", "welcome"]);
+	const [name, onChangeName] = useState("");
+	const [email, onChangeEmail] = useState("");
+	const [password, onChangePassword] = useState("");
 	const {
 		colors,
 		variant,
@@ -45,7 +47,7 @@ function Example() {
 	const [currentId, setCurrentId] = useState(-1);
 
 	const { isSuccess, data, isFetching } = useQuery({
-		queryKey: ['example', currentId],
+		queryKey: ["example", currentId],
 		queryFn: () => {
 			return fetchOne(currentId);
 		},
@@ -54,25 +56,26 @@ function Example() {
 
 	useEffect(() => {
 		if (isSuccess) {
-			Alert.alert(t('example:welcome', data.name));
+			Alert.alert(t("example:welcome", data.name));
 		}
 	}, [isSuccess, data]);
 
-	const onChangeTheme = () => {
-		changeTheme(variant === 'default' ? 'dark' : 'default');
+	const onPressCreate = async () => {
+		const response  = await fetch(`${API_URL}/auth/register`, {
+			method: 'POST',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				name,
+				email,
+				password
+			}),
+		})
+		if(response.status === 200) Alert.alert('Create Account Success!')
+		if(response.status === 400) Alert.alert('Create Account Failed!')
 	};
-
-	const onChangeLanguage = (lang: 'fr' | 'en') => {
-		void i18next.changeLanguage(lang);
-	};
-
-	if (
-		!isImageSourcePropType(SendImage) ||
-		!isImageSourcePropType(ColorsWatchImage) ||
-		!isImageSourcePropType(TranslateImage)
-	) {
-		throw new Error('Image source is not valid');
-	}
 
 	return (
 		<SafeScreen>
@@ -84,96 +87,79 @@ function Example() {
 						gutters.marginTop_80,
 					]}
 				>
-						<Text style={[fonts.size_40, fonts.gray800, fonts.bold]}>
-							Create Account
-						</Text>
+					<Text style={[fonts.size_40, fonts.gray800, fonts.bold]}>
+						Create Account
+					</Text>
 				</View>
 
 				<View style={[gutters.paddingHorizontal_32, gutters.marginTop_40]}>
 					<View style={[layout.justifyCenter]}>
-					<TextInput
-						style={[gutters.marginVertical_12, gutters.padding_12, {height: 40, borderWidth: 1}]}
-						onChangeText={onChangeName}
-						placeholder="Fullname"
-						value={name}
-					/>	
-					<TextInput
-						style={[gutters.marginVertical_12, gutters.padding_12, {height: 40, borderWidth: 1}]}
-						onChangeText={onChangeEmail}
-						value={email}
-						placeholder="Email"
-					/>
-					<SelectDropdown
-						buttonStyle={[gutters.marginVertical_12, layout.fullWidth, {height: 40, borderWidth: 1}]}
-						data={genders}
-						onSelect={(selectedItem, index) => {
-							console.log(selectedItem, index)
-						}}
-						buttonTextAfterSelection={(selectedItem, index) => {
-							// text represented after item is selected
-							// if data array is an array of objects then return selectedItem.property to render after item is selected
-							return selectedItem
-						}}
-						rowTextForSelection={(item, index) => {
-							// text represented for each item in dropdown
-							// if data array is an array of objects then return item.property to represent item in dropdown
-							return item
-						}}
-					/>
-					<TextInput
-						style={[gutters.marginVertical_12, gutters.padding_12, {height: 40, borderWidth: 1}]}
-						onChangeText={onChangePassword}
-						value={password}
-						placeholder="Password"
-					/>
+						<TextInput
+							style={[
+								gutters.marginVertical_12,
+								gutters.padding_12,
+								{ height: 40, borderWidth: 1 },
+							]}
+							onChangeText={onChangeName}
+							placeholder="Fullname"
+							value={name}
+						/>
+						<TextInput
+							style={[
+								gutters.marginVertical_12,
+								gutters.padding_12,
+								{ height: 40, borderWidth: 1 },
+							]}
+							onChangeText={onChangeEmail}
+							value={email}
+							placeholder="Email"
+						/>
+						<SelectDropdown
+							buttonStyle={[
+								gutters.marginVertical_12,
+								layout.fullWidth,
+								{ height: 40, borderWidth: 1 },
+							]}
+							data={genders}
+							onSelect={(selectedItem, index) => {
+								console.log(selectedItem, index);
+							}}
+							buttonTextAfterSelection={(selectedItem, index) => {
+								// text represented after item is selected
+								// if data array is an array of objects then return selectedItem.property to render after item is selected
+								return selectedItem;
+							}}
+							rowTextForSelection={(item, index) => {
+								// text represented for each item in dropdown
+								// if data array is an array of objects then return item.property to represent item in dropdown
+								return item;
+							}}
+						/>
+						<TextInput
+							style={[
+								gutters.marginVertical_12,
+								gutters.padding_12,
+								{ height: 40, borderWidth: 1 },
+							]}
+							onChangeText={onChangePassword}
+							value={password}
+							placeholder="Password"
+						/>
 					</View>
 
 					<View
 						style={[
-							layout.row,
-							layout.justifyBetween,
+							layout.justifyCenter,
 							layout.fullWidth,
 							gutters.marginTop_16,
 						]}
 					>
-						<TouchableOpacity
-							testID="fetch-user-button"
-							style={[components.buttonCircle, gutters.marginBottom_16]}
-							onPress={() => setCurrentId(Math.ceil(Math.random() * 10 + 1))}
-						>
-							{isFetching ? (
-								<ActivityIndicator />
-							) : (
-								<ImageVariant
-									source={SendImage}
-									style={{ tintColor: colors.purple500 }}
-								/>
-							)}
-						</TouchableOpacity>
-
-						<TouchableOpacity
-							testID="change-theme-button"
-							style={[components.buttonCircle, gutters.marginBottom_16]}
-							onPress={() => onChangeTheme()}
-						>
-							<ImageVariant
-								source={ColorsWatchImage}
-								style={{ tintColor: colors.purple500 }}
-							/>
-						</TouchableOpacity>
-
-						<TouchableOpacity
-							testID="change-language-button"
-							style={[components.buttonCircle, gutters.marginBottom_16]}
-							onPress={() =>
-								onChangeLanguage(i18next.language === 'fr' ? 'en' : 'fr')
-							}
-						>
-							<ImageVariant
-								source={TranslateImage}
-								style={{ tintColor: colors.purple500 }}
-							/>
-						</TouchableOpacity>
+						<Button
+							onPress={onPressCreate}
+							title="Create"
+							color={colors.gray800}
+							accessibilityLabel="Learn more about this purple button"
+						/>
 					</View>
 				</View>
 			</ScrollView>
